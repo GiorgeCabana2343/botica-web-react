5a6b465368cf799343cbff5bca7f3dcdaf184c6a
Zambrano777 <doubledamage84@gmail.com>
2025-10-26 15:27:11 -0500

    se agrega redirect y outdir

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
+  // Esta opción fuerza un slash al final de todas las rutas
+  // útil para React Router en Vercel
+  server: { fs: { strict: false } }
 })
